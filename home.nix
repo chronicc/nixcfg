dffca5c52eca9fbe5a631fbfb0e3b47ce0dce2cb
Thomas Steinert <hello@chroni.cc>
2023-04-19 13:30:09 +0200

    Add kitty configuration

--- home.nix
+++ home.nix
@@ -6,9 +6,9 @@
     awscli2
     brave
     docker
-    kitty
     minikube
     poetry
+    source-code-pro
     tig
     terraform
   ];
@@ -68,6 +68,30 @@
 
   programs.jq = {
     enable = true;
+  };
+
+  programs.kitty = {
+    enable = true;
+    font = {
+      name = "Source Code Pro";
+      size = 10;
+    };
+    keybindings = {
+      "ctrl+shift+up" = "change_font_size all +2.0";
+      "ctrl+shift+down" = "change_font_size all -2.0";
+      "ctrl+shift+backspace" = "change_font_size all 0";
+    };
+    settings = {
+      background_opacity = "0.9";
+      enable_audio_bell = "no";
+      placement_stragegy = "top-left";
+      scrollback_lines = "5000";
+      visual_bell_duration = "0.0";
+      window_border_width = "0";
+      window_margin_width = "4";
+      window_padding_width = "0";
+    };
+    theme = "Gruvbox Dark";
   };
 
   programs.neovim = {
