aba73f6adf3e3a70e5051102ad5c4b071df67385
Thomas Steinert <hello@chroni.cc>
2023-06-02 16:34:05 +0200

    feat: add terminator as terminal

--- flake.nix
+++ flake.nix
@@ -89,6 +89,7 @@
                   ./modules/desktop/swaylock
 
                   ./modules/terminals/kitty
+                  ./modules/terminals/terminator
                 ];
               };
             }
