e79f53c3ea56cecfcc26ddbd6f645fe42a510b5a
Thomas Steinert <hello@chroni.cc>
2023-06-18 17:32:40 +0200

    feat: add swap to vps2.kurthos.com

--- hosts/vps2.kurthos.com/hardware-configuration.nix
+++ hosts/vps2.kurthos.com/hardware-configuration.nix
@@ -20,7 +20,12 @@
     fsType = "ext4";
   };
 
-  swapDevices = [ ];
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 32*1024;
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
