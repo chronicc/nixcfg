49e7eb4f0118335e273da71927ce3855f3916b1d
Thomas Steinert <hello@chroni.cc>
2023-06-18 15:26:50 +0200

    chore: add allowed ssh key to root user

---
+++ users/root/default.nix
@@ -0,0 +1,6 @@
+{ ... }:
+{
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBPjRqwfLVi3GXXN97ZlJGdefYv2/HG6iTKOeG+Yqa+1qM82Y/MLyzrNTS1+wlmIB9fPBW3IsIhJtxR9Rw8cj65c= hello@chroni.cc"
+  ];
+}
