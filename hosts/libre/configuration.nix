5c901394817de9fe50732ef76a46c9e183943799
Thomas Steinert <hello@chroni.cc>
2023-06-18 15:25:07 +0200

    refactor: switch to flat structure and add variables for libres configuration

--- hosts/libre/configuration.nix
+++ hosts/libre/configuration.nix
@@ -4,7 +4,10 @@
 
 { config, inputs, lib, pkgs, ... }:
 
-{
+let
+  config.flake.location = "/home/chronicc/git/chronicc/flakes/flake.nix";
+  config.timezone = "Europe/Berlin";
+in {
   imports =
     [
       ../../users/chronicc
@@ -14,58 +17,41 @@
       ../../modules/peripherie/printers
     ];
 
-  boot = {
-    initrd = {
-      luks.devices = {
-        crypted = {
-          bypassWorkqueues = true;
-          device = "/dev/disk/by-partuuid/c952e698-b368-42b0-a43c-aaa890e73ea6";
-          preLVM = true;
-        };
-      };
-      systemd.enable = true;
-    };
-    kernelPackages = pkgs.linuxPackages_latest;
-    kernelParams = [
-      "boot.shell_on_fail"
-      "i915.force_probe=46a6"
-      "quiet"
-    ];
-    loader = {
-      efi.canTouchEfiVariables = true;
-      systemd-boot = {
-        configurationLimit = 10;
-        enable = true;
-      };
-      timeout = 1;
-    };
-    plymouth = {
-      enable = true;
-    };
+  boot.initrd.luks.devices.crypted = {
+    bypassWorkqueues = true;
+    device = "/dev/disk/by-partuuid/c952e698-b368-42b0-a43c-aaa890e73ea6";
+    preLVM = true;
   };
+  boot.initrd.systemd.enable = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelParams = [
+    "boot.shell_on_fail"
+    "i915.force_probe=46a6"
+    "quiet"
+  ];
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.timeout = 1;
+  boot.plymouth.enable = true;
 
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "de-latin1-nodeadkeys";
-  };
+  console.font = "Lat2-Terminus16";
+  console.keyMap = "de-latin1-nodeadkeys";
 
-  environment = {
-    etc = {
-    };
-    systemPackages = with pkgs; [
-      bottom
-      dig
-      git
-      git-lfs
-      killall
-      nix-index
-      pciutils
-      usbutils
-      vim
-      wget
-      xdg-utils
-    ];
-  };
+  environment.systemPackages = with pkgs; [
+    bottom
+    dig
+    git
+    git-lfs
+    killall
+    nix-index
+    pciutils
+    usbutils
+    vim
+    virt-manager
+    wget
+    xdg-utils
+  ];
 
   fonts.fonts = with pkgs; [
     carlito
@@ -79,39 +65,31 @@
     })
   ];
 
-  hardware = {
-    opengl = {
-      driSupport32Bit = true;
-      enable = true;
-      extraPackages = with pkgs; [
-        intel-media-driver
-        libvdpau-va-gl
-        vaapiIntel
-        vaapiVdpau
-      ];
-    };
+  hardware.opengl.driSupport32Bit = true;
+  hardware.opengl.enable = true;
+  hardware.opengl.extraPackages = with pkgs; [
+    intel-media-driver
+    libvdpau-va-gl
+    vaapiIntel
+    vaapiVdpau
+  ];
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
   };
 
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_TIME = "de_DE.UTF-8";
-      LC_MONETARY = "de_DE.UTF-8";
-    };
-  };
-
-  networking = {
-    extraHosts = ''
-      127.0.0.1 my-app-frontend.local my-app-backend.local
-    '';
-    hostName = "libre";
-    networkmanager = {
-      enable = true;
-      insertNameservers = ["8.8.8.8" "8.8.4.4"];
-    };
-    wireless.userControlled.enable = true;
-  };
-
+  networking.extraHosts = ''
+    127.0.0.1 my-app-frontend.local my-app-backend.local
+  '';
+  networking.hostName = "libre";
+  networking.networkmanager.enable = true;
+  networking.networkmanager.insertNameservers = ["8.8.8.8" "8.8.4.4"];
+  networking.wireless.userControlled.enable = true;
 
   nix.gc.automatic = true;
   nix.gc.dates = "weekly";
@@ -119,20 +97,14 @@
   nix.package = pkgs.nixFlakes;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  security = {
-    sudo = {
-      wheelNeedsPassword = false;
-    };
-  };
+  security.sudo.wheelNeedsPassword = false;
 
   services.locate.enable = true;
-
   services.pipewire.enable = true;
   services.pipewire.alsa.enable = true;
   services.pipewire.alsa.support32Bit = true;
   services.pipewire.pulse.enable = true;
   services.pipewire.wireplumber.enable = true;
-
   services.xserver.autorun = true;
   services.xserver.enable = true;
   services.xserver.desktopManager.mate.enable = true;
@@ -141,16 +113,12 @@
   services.xserver.libinput.enable = true;
   services.xserver.xkbVariant = "nodeadkeys";
 
-  sound = {
-    enable = true;
-    mediaKeys = {
-      enable = true;
-    };
-  };
+  sound.enable = true;
+  sound.mediaKeys.enable = true;
 
   systemd.tmpfiles.rules = [
     "L /bin/bash - - - - ${pkgs.bash}/bin/bash"
-    "L /etc/nixos/flake.nix - - - - /home/chronicc/git/chronicc/flakes/flake.nix"
+    "L /etc/nixos/flake.nix - - - - ${config.flake.location}"
   ];
 
   # This value determines the NixOS release from which the default
@@ -161,16 +129,7 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
 
-  time.timeZone = "Europe/Berlin";
-
-  users.users.chronicc = {
-    extraGroups = [ "audio" "docker" "lp" "networkmanager" "scanner" "video" "wheel" ];
-    initialPassword = "password";
-    isNormalUser = true;
-    openssh.authorizedKeys.keys = [
-      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBPjRqwfLVi3GXXN97ZlJGdefYv2/HG6iTKOeG+Yqa+1qM82Y/MLyzrNTS1+wlmIB9fPBW3IsIhJtxR9Rw8cj65c= hello@chroni.cc"
-    ];
-  };
+  time.timeZone = "${config.timezone}";
 
   virtualisation.docker.enable = true;
 }
