861982875f7f23a519c9f4a88252f15c8cf817cb
Thomas Steinert <hello@chroni.cc>
2023-07-03 21:04:53 +0200

    refactor: consolidate package channels and version overwrites

--- flake.nix
+++ flake.nix
@@ -9,59 +9,48 @@
     nixpkgsMaster.url                   = "github:nixos/nixpkgs/master";
   };
 
-  outputs = inputs@{ self, home-manager, nixpkgs, nixpkgsUnstable, nixpkgsMaster, ... }:
+  outputs = inputs@{ self, ... }:
     let
-      lib = nixpkgs.lib;
+      lib = inputs.nixpkgs.lib;
       system = "x86_64-linux";
 
-      pkgs = import nixpkgs {
+      pkgsUnstable = import inputs.nixpkgsUnstable {
         inherit system;
         config.allowUnfree = true;
       };
 
-      pkgsUnstable = import nixpkgsUnstable {
+      pkgsMaster = import inputs.nixpkgsMaster {
         inherit system;
         config.allowUnfree = true;
       };
 
-      pkgsMaster = import nixpkgsMaster {
+      pkgs = import inputs.nixpkgs {
         inherit system;
         config.allowUnfree = true;
+        overlays = [
+          (self: super: {
+            qt6Packages.qt6ct = pkgsUnstable.qt6Packages.qt6ct;
+            qt6Packages.qtstyleplugin-kvantum = pkgsUnstable.qt6Packages.qtstyleplugin-kvantum;
+            skaffold = pkgsUnstable.skaffold;
+            vscode = pkgsUnstable.vscode;
+          })
+        ];
       };
     in {
-      # Pull these packages from different branches
-      overridePackages = self: super: {
-        qt6Packages.qt6ct = pkgsUnstable.qt6Packages.qt6ct;
-        qt6Packages.qtstyleplugin-kvantum = pkgsUnstable.qt6Packages.qtstyleplugin-kvantum;
-        skaffold = pkgsUnstable.skaffold;
-        vscode = pkgsUnstable.vscode;
-      };
-
       nixosConfigurations = {
         libre = lib.nixosSystem {
           inherit system;
-          specialArgs = { inherit inputs; };
+          specialArgs = { inherit inputs pkgs; };
           modules = [
             ./hosts/libre
-            {
-              nixpkgs.overlays = [ self.overridePackages ];
-              nixpkgs.config.allowUnfree = true;
-            }
-
-
-
           ];
         };
 
         "vps2.kurthos.com" = lib.nixosSystem {
           inherit system;
-          specialArgs = { inherit inputs; };
+          specialArgs = { inherit inputs pkgs; };
           modules = [
             ./hosts/vps2.kurthos.com
-            {
-              nixpkgs.overlays = [ self.overridePackages ];
-              nixpkgs.config.allowUnfree = true;
-            }
           ];
         };
       };
