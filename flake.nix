f0242381665e74735fb2c5e0e5993d0be488843c
Thomas Steinert <hello@chroni.cc>
2023-07-03 21:20:52 +0200

    fix: pin home-manager to release 23.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url                    = "github:nix-community/home-manager";
+    home-manager.url                    = "github:nix-community/home-manager/release-23.05";
     nixpkgs.url                         = "github:nixos/nixpkgs/nixos-23.05";
     nixpkgsUnstable.url                 = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgsMaster.url                   = "github:nixos/nixpkgs/master";
